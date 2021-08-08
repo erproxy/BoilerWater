0e286ffd26aaeb9b22231f8262c8ea0585e5c559
erproxy <69542801+erproxy@users.noreply.github.com>
2021-08-08 19:36:42 +0400

    SwipeMenuBottlesV2
    Добавил возможность вытягивания предметов из бара предметов

---
+++ BoilerWater/Assets/Scripts/Ui/Lvl/DragElement.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragElement : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+    [SerializeField] private Sprite _mainImage;
+    
+    private Transform _transformSelf;
+    private RectTransform _rectTransform;
+    public Canvas Canvas;
+
+
+    private void Start()
+    {
+        _rectTransform = GetComponent<RectTransform>();
+        _transformSelf = transform;
+    }
+
+    /// <summary>
+    /// Материал, применяемый к объектам на сцене
+    /// </summary>
+    public Sprite MainImage
+    {
+        get { return _mainImage; }
+        set
+        {
+            if (value != null)
+            {
+                _mainImage = value;
+            }
+        }
+    }
+
+    private Transform defaultParentTransform;
+    /// <summary>
+    /// Трансформ объекта, к которому прикреплена кнопка
+    /// </summary>
+    public Transform DefaultParentTransform
+    {
+        get { return defaultParentTransform; }
+        set
+        {
+            if (value != null)
+            {
+                defaultParentTransform = value;
+            }
+        }
+    }
+
+    private Transform dragParentTransform;
+    /// <summary>
+    /// Трансформ объекта, к которому прикрепляется кнопка во время драга
+    /// </summary>
+    public Transform DragParentTransform
+    {
+        get
+        {
+            return dragParentTransform;
+        }
+        set
+        {
+            if (value != null)
+                dragParentTransform = value;
+        }
+    }
+
+    private int siblingIndex;
+    /// <summary>
+    /// Номер индекса внутри родительского объекта
+    /// </summary>
+    public int SiblingIndex
+    {
+        get { return siblingIndex; }
+        set
+        {
+            if (value > 0)
+                siblingIndex = value;
+        }
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        _transformSelf.SetParent(DragParentTransform);
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        _rectTransform.anchoredPosition += eventData.delta / Canvas.scaleFactor;
+        // transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1));
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        //возвращаем обратно в контент элемент
+        transform.SetParent(DefaultParentTransform);
+        transform.SetSiblingIndex(SiblingIndex);
+
+        // //создаем луч и хит
+        // Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        // RaycastHit hit;
+
+        // //пускаем луч и меняем материал у встреченного объекта
+        // if (Physics.Raycast(ray, out hit))
+        // {
+        //     hit.collider.gameObject.GetComponent<Renderer>().material = mainMaterial;
+        // }
+    }
+
+}
---
+++ BoilerWater/Assets/Scripts/Ui/Lvl/DragPanel.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DragPanel : MonoBehaviour {
+
+    [Tooltip("Ссылка на префаб драгуемого элемента")]
+    [SerializeField] private GameObject _dragElementPrefab;
+
+    [Tooltip("Ссылка на Content ScrollView")]
+    [SerializeField] private Transform _scrollViewContent;
+
+    [Tooltip("Спрайти для замены спрайтов в объектах")]
+    [SerializeField] private List<Sprite> _sprites;
+
+    [SerializeField] private Canvas _canvas;
+
+    private void Start()
+    {
+        var transformSelf = transform;
+        for (int i = 0; i < _sprites.Count; ++i)
+        {
+            var dragObject = Instantiate(_dragElementPrefab, _scrollViewContent);
+            var script = dragObject.GetComponent<DragElement>();
+            
+            dragObject.GetComponent<Image>().sprite = _sprites[i];
+            script.DefaultParentTransform = _scrollViewContent;
+            script.DragParentTransform = transformSelf;
+            script.Canvas = _canvas;
+            script.SiblingIndex = i;
+        }
+    }
+
+}
